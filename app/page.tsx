6c734100681e55e6d51a71532f12a15aeeb1d4fe
Andrew Aliaj <andyaliaj@gmail.com>
2024-05-10 08:45:16 -0400

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+
+
+export default function Home() {
+  return (
+    <main className="    bg-blue-400 min-h-screen">
+    <div className=" text-center">
+ <h1 className=" text-yellow-500 text-4xl  bg-gray-700"> Hello World</h1>
+
+
+    </div>
+    </main>
+  );
+}
